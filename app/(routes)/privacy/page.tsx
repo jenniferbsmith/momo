1d2959b21005f1cb8e055679258df6117eac4af0
jenniferbsmith <jenniferbsmith900@gmail.com>
2025-08-22 22:24:40 +0500

    Update page.tsx

--- app/(routes)/privacy/page.tsx
+++ app/(routes)/privacy/page.tsx
@@ -233,7 +233,7 @@ To exercise these rights, contact us at privacy@text-behind-image.io with your r
                 
                 <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
                   <Button asChild variant="outline" size="lg" className="border-primary/50 hover:bg-primary/10">
-                    <Link href="/welcome">
+                    <Link href="/app">
                       <Wand2 className="w-5 h-5 mr-2" />
                       Try Text Behind Image
                     </Link>
